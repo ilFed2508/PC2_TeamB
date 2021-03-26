d10c31f3fbbf17024ce0753b9ce76400d362675f
Joe Abiad <Joenielscrs@gmail.com>
2021-03-26 18:25:09 +0100

    Camera Animation
    Animation between Main Menu and Settings

---
+++ ProjectTeamB_PC2/Assets/Scripts/Ui/Timeline/CameraOnSettings.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CameraOnSettings : MonoBehaviour
+{
+    public PlayableDirector SetingsTimeline;
+
+    void start()
+    {
+        SetingsTimeline = GetComponent<PlayableDirector>();
+    }
+
+    public void StartTimeline()
+    {
+        {
+            SetingsTimeline.Play();
+        }
+    }
+}
