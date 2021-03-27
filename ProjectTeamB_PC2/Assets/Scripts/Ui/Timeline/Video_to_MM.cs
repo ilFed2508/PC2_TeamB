401e7d75a0ae6b1f594ecd1aed7f17686b4a9b59
Joe Abiad <Joenielscrs@gmail.com>
2021-03-27 20:51:44 +0100

    Camera Animation Added
    - Sound&Controls to Video and Video back to Main Menu

---
+++ ProjectTeamB_PC2/Assets/Scripts/Ui/Timeline/Video_to_MM.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Video_to_MM : MonoBehaviour
+{
+    public PlayableDirector Video_to_MainMenu;
+
+    void start()
+    {
+        Video_to_MainMenu = GetComponent<PlayableDirector>();
+    }
+
+    public void StartTimeline()
+    {
+        {
+            Video_to_MainMenu.Play();
+        }
+    }
+}
