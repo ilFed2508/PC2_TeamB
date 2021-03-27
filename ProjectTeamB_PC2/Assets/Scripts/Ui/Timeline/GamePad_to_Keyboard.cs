f16e77dc2169e8e120adfa4bba42facc5c4f6e2c
Joe Abiad <Joenielscrs@gmail.com>
2021-03-27 14:30:02 +0100

    Camera Animation Added
    - GamePad scheme to Keyboard scheme

---
+++ ProjectTeamB_PC2/Assets/Scripts/Ui/Timeline/GamePad_to_Keyboard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class GamePad_to_Keyboard : MonoBehaviour
+{
+    public PlayableDirector Pad_to_Keyboard;
+
+    void start()
+    {
+        Pad_to_Keyboard = GetComponent<PlayableDirector>();
+    }
+
+    public void StartTimeline()
+    {
+        {
+            Pad_to_Keyboard.Play();
+        }
+    }
+}
