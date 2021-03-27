7c0ace93d6442ffd8d3e019f15a9da9f8a3c129c
Joe Abiad <Joenielscrs@gmail.com>
2021-03-27 02:07:39 +0100

    Scene Updated
    - Added Controls Settings + camera animation

---
+++ ProjectTeamB_PC2/Assets/Scripts/Ui/Timeline/Controls_to_MM.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Controls_to_MM : MonoBehaviour
+{
+    public PlayableDirector Controls_to_Main;
+
+    void start()
+    {
+        Controls_to_Main = GetComponent<PlayableDirector>();
+    }
+
+    public void StartTimeline()
+    {
+        {
+            Controls_to_Main.Play();
+        }
+    }
+}
