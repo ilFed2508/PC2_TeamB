014a760afa9d6a755e52399a8786b2a1655efe9d
Joe Abiad <Joenielscrs@gmail.com>
2021-03-27 21:34:28 +0100

    Camera Animation Added
    - Video to Sound and Controls

---
+++ ProjectTeamB_PC2/Assets/Scripts/Ui/Timeline/Video_to_Sound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Video_to_Sound : MonoBehaviour
+{
+    public PlayableDirector Video_to_SoundSettins;
+
+    void start()
+    {
+        Video_to_SoundSettins = GetComponent<PlayableDirector>();
+    }
+
+    public void StartTimeline()
+    {
+        {
+            Video_to_SoundSettins.Play();
+        }
+    }
+}
