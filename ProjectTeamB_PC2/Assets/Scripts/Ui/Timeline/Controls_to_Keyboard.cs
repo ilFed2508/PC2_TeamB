d907d43df20e97ed4d5906bb5a6918ebd51c0a2c
Joe Abiad <Joenielscrs@gmail.com>
2021-03-27 13:32:48 +0100

    Camera Animation Added
    - Controls to Keyboard scheme and Keyboard scheme back to Controls

---
+++ ProjectTeamB_PC2/Assets/Scripts/Ui/Timeline/Controls_to_Keyboard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Controls_to_Keyboard : MonoBehaviour
+{
+    public PlayableDirector Controls_to_KeyMouse;
+
+    void start()
+    {
+        Controls_to_KeyMouse = GetComponent<PlayableDirector>();
+    }
+
+    public void StartTimeline()
+    {
+        {
+            Controls_to_KeyMouse.Play();
+        }
+    }
+}
---
+++ ProjectTeamB_PC2/Assets/Scripts/Ui/Timeline/Keyboard_to_Controls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Keyboard_to_Controls : MonoBehaviour
+{
+    public PlayableDirector KeyMouse_to_Controls;
+
+    void start()
+    {
+        KeyMouse_to_Controls = GetComponent<PlayableDirector>();
+    }
+
+    public void StartTimeline()
+    {
+        {
+            KeyMouse_to_Controls.Play();
+        }
+    }
+}
