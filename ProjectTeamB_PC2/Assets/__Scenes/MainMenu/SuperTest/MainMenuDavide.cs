06c58f99f6e90a20700f3b3f8580606998ef1a61
Perdons22 <davide.perdon22@gmail.com>
2021-03-29 20:56:12 +0200

    Menu experiment

---
+++ ProjectTeamB_PC2/Assets/__Scenes/MainMenu/SuperTest/MainMenuDavide.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuDavide : MonoBehaviour
+{
+    public Animator cameraSuper;
+
+    public void MenuPrincipale()
+    {
+        cameraSuper.SetInteger("Page", 0);
+    }
+
+    public void Opzioni()
+    {
+        cameraSuper.SetInteger("Page", 1);
+    }
+
+    public void OpzioniControlli()
+    {
+        cameraSuper.SetInteger("Page", 2);
+    }
+
+    public void OpzioniVideo()
+    {
+        cameraSuper.SetInteger("Page", 3);
+    }
+
+    public void Tastiera()
+    {
+        cameraSuper.SetInteger("Page", 4);
+    }
+
+    public void Controller()
+    {
+        cameraSuper.SetInteger("Page", 5);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
