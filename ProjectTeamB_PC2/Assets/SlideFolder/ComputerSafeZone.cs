028942804c89898102661cc653e465401e677934
LucaDL <lanzellottiluca9800@gmail.com>
2021-04-16 21:56:51 +0200

    Add - Slide and PC

---
+++ ProjectTeamB_PC2/Assets/SlideFolder/ComputerSafeZone.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputerSafeZone : MonoBehaviour
+{
+    
+    private PlayerController EpickUp;
+    private SlideManager marketPanel;
+    // Start is called before the first frame update
+    void Update()
+    {
+       
+        EpickUp = FindObjectOfType<PlayerController>();
+        marketPanel = FindObjectOfType<SlideManager>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            EpickUp.PickUp.SetActive(true);
+
+        }
+        if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+        {          
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            marketPanel.MarketPanel.SetActive(true);
+            EpickUp.PickUp.SetActive(false);
+            Time.timeScale = 0f;           
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            EpickUp.PickUp.SetActive(false);
+
+        }
+    }
+}
---
+++ ProjectTeamB_PC2/Assets/SlideFolder/SlideManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideManager : MonoBehaviour
+{
+    private CharacterController controller;
+    public float slideSpeed;
+    public bool isSliding;
+
+    public float TimeSlide;
+    public float TimeToSlide;
+
+    public float CopyTimeSlide;
+    public float CopyTimeToSlide;
+
+    private Animator weaponSlotCam;
+
+    public GameObject MarketPanel;
+   
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        weaponSlotCam = GameObject.Find("WeaponSlot").GetComponent<Animator>();
+        isSliding = true;
+    }
+
+    
+    void Update()
+    {     
+        if (isSliding && TimeSlide>0f && controller.isGrounded)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                Slide();
+            }
+        }
+        if(TimeSlide <= 0f)
+        {
+            TimeToSlide -= Time.deltaTime;
+            if(TimeToSlide <= 0f)
+            {
+                TimeSlide = CopyTimeSlide;
+                TimeToSlide = CopyTimeToSlide;
+            }
+        }
+    } 
+    
+
+    public void Slide()
+    {
+        
+
+        TimeSlide -= Time.deltaTime;
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        
+        Vector3 Lateral = transform.TransformDirection(Vector3.right);
+       
+
+        float curSpeed = slideSpeed * Input.GetAxis("Vertical");
+        float LatSpeed = slideSpeed * Input.GetAxis("Horizontal");
+
+        controller.SimpleMove(forward * curSpeed);
+        controller.SimpleMove(Lateral * LatSpeed);        
+       
+
+        if (curSpeed > 0f)
+        {
+            weaponSlotCam.Play("SlideFarward");
+        }
+        if(-curSpeed > -0f)
+        {
+            weaponSlotCam.Play("SlideBack");
+        }
+        if(LatSpeed > 0f)
+        {
+            weaponSlotCam.Play("RightSlide");           
+        }
+        if (-LatSpeed > -0f)
+        {
+            weaponSlotCam.Play("LeftSlide");
+        }
+    }
+}
