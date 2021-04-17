ab1c8e44e60bc29c102aa8b2d4fa9b5239e1f558
Joe Abiad <Joenielscrs@gmail.com>
2021-04-17 14:24:48 +0200

    Scene Update
    Back Button (level selection to main menu) added

--- ProjectTeamB_PC2/Assets/__Scenes/MainMenu/SuperTest/ComputerLevelSelection.cs
+++ ProjectTeamB_PC2/Assets/__Scenes/MainMenu/SuperTest/ComputerLevelSelection.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class ComputerLevelSelection : MonoBehaviour
 {
-    public GameObject levelselectionCanvas;
+    public GameObject levelselectionCanvas, backCanvas;
 
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("Player") && Input.GetKey(KeyCode.E))
         {
             levelselectionCanvas.SetActive(true);
+            backCanvas.SetActive(false);
         }
     }
 }
