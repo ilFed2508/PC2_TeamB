4a427a37ee285ca9c707de7c68d735c2a5ac33a9
Joe Abiad <Joenielscrs@gmail.com>
2021-04-17 14:04:41 +0200

    Test_level selection canvas updted + animation

---
+++ ProjectTeamB_PC2/Assets/__Scenes/MainMenu/SuperTest/ComputerLevelSelection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerLevelSelection : MonoBehaviour
+{
+    public GameObject levelselectionCanvas;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && Input.GetKey(KeyCode.E))
+        {
+            levelselectionCanvas.SetActive(true);
+        }
+    }
+}
