bc37ba5807adaeb192695ab4a60a5264b861edb0
Joe Abiad <Joenielscrs@gmail.com>
2021-04-18 16:03:12 +0200

    SafeZone Panel addded

---
+++ ProjectTeamB_PC2/Assets/Scripts/OpenPanelSafe.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenPanelSafe : MonoBehaviour
+{
+
+    public GameObject SafePanel;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Open"))
+        {
+            SafePanel.SetActive(true);
+            StartCoroutine(MyCoroutine());
+        }
+
+    }
+
+    // Update is called once per frame
+    IEnumerator MyCoroutine()
+    {
+        yield return new WaitForSeconds(2.0f);
+        SafePanel.SetActive(false);
+    }
+}
