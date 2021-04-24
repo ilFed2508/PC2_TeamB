72f0d46a7e0a21f1d0872e68915a0500a3b6a3e9
Perdons22 <davide.perdon22@gmail.com>
2021-04-24 16:20:59 +0200

    Tutorial Corrections

--- ProjectTeamB_PC2/Assets/DesignScripts/PlayerSpawn.cs
+++ ProjectTeamB_PC2/Assets/DesignScripts/PlayerSpawn.cs
@@ -7,23 +7,28 @@ public class PlayerSpawn : MonoBehaviour
     public GameObject player;
     public GameObject spawn1, spawn2;
 
+    private DetectedActDeact hudOn;
+
     // Start is called before the first frame update
     void Start()
+    {
+        Spawn();
+    }
+
+    private void Spawn()
     {
         if (PlayerPrefs.GetInt("Checkpoint") == 1)
         {
             Vector3 position1 = new Vector3(spawn1.transform.position.x, spawn1.transform.position.y, spawn1.transform.position.z);
             Instantiate(player, position1, spawn2.transform.rotation);
-
-            //player.transform.position = spawn1.transform.position;
         }
 
         if (PlayerPrefs.GetInt("Checkpoint") == 2)
         {
             Vector3 position2 = new Vector3(spawn2.transform.position.x, spawn2.transform.position.y, spawn2.transform.position.z);
             Instantiate(player, position2, spawn2.transform.rotation);
-
-            //player.transform.position = spawn2.transform.position;
+            hudOn = GameObject.Find("HUD").GetComponent<DetectedActDeact>();
+            hudOn.thing.SetActive(true);
         }
     }
 }
