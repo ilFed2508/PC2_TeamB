c0d43d90130bd6fe0a8715d7f6fafc2e0f6dd6fe
Perdons22 <davide.perdon22@gmail.com>
2021-05-07 18:41:10 +0200

    LevelAll

--- ProjectTeamB_PC2/Assets/LevelDesign/Scripts/SafeZone.cs
+++ ProjectTeamB_PC2/Assets/LevelDesign/Scripts/SafeZone.cs
@@ -7,9 +7,6 @@ public class SafeZone : MonoBehaviour
     [Header("Maps")]
     public GameObject mapIn;
     public GameObject mapOut;
-    [Header("Safes")]
-    public GameObject safeIn;
-    public GameObject safeOut;
     [Header("Other Things")]
     public float recoverdLife;
     public int checkpoint;
@@ -34,14 +31,8 @@ public class SafeZone : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             mapOut.SetActive(true);
+            mapIn.SetActive(false);
             PlayerPrefs.SetInt("Checkpoint", checkpoint);
         }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        mapIn.SetActive(false);
-        safeIn.SetActive(false);
-        safeOut.SetActive(true);
     }
 }
