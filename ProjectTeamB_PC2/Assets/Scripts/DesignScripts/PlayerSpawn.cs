ae5697636617ac57f1b9dd89c9feb7c728af9f46
crystaldevaster <richicryst@gmail.com>
2021-05-24 17:30:45 +0200

    godMode Fix pt1

--- ProjectTeamB_PC2/Assets/Scripts/DesignScripts/PlayerSpawn.cs
+++ ProjectTeamB_PC2/Assets/Scripts/DesignScripts/PlayerSpawn.cs
@@ -6,6 +6,8 @@ public class PlayerSpawn : MonoBehaviour
 {
     public GameObject player;
     public GameObject playerDisarmato;
+    public GameObject playerGOD;
+    public GameObject playerDisarmatoGOD;
     public GameObject spawn1, spawn2, spawn3;
 
     private DetectedActDeact hudOn;
@@ -21,7 +23,17 @@ public class PlayerSpawn : MonoBehaviour
         if (PlayerPrefs.GetInt("Checkpoint") == 1)
         {
             Vector3 position1 = new Vector3(spawn1.transform.position.x, spawn1.transform.position.y, spawn1.transform.position.z);
-            GameObject PlayerSpawned = Instantiate(playerDisarmato, position1, spawn1.transform.rotation);
+            GameObject PlayerSpawned;
+            if (PlayerPrefs.GetInt("GodmodeActivated") == 1)
+            {
+                //godmodeAttiva
+                PlayerSpawned = Instantiate(playerDisarmatoGOD, position1, spawn1.transform.rotation);
+            }
+            else
+            {
+                //GodmodeDisattiva
+                PlayerSpawned = Instantiate(playerDisarmato, position1, spawn1.transform.rotation);
+            }           
             //reset score
             ScoreController scoreController = PlayerSpawned.GetComponent<ScoreController>();
             if(PlayerPrefs.GetInt("PlayerTotalScore") != 0)
@@ -35,7 +47,17 @@ public class PlayerSpawn : MonoBehaviour
         if (PlayerPrefs.GetInt("Checkpoint") == 2)
         {
             Vector3 position2 = new Vector3(spawn2.transform.position.x, spawn2.transform.position.y, spawn2.transform.position.z);
-            GameObject PlayerSpawned = Instantiate(player, position2, spawn2.transform.rotation);
+            GameObject PlayerSpawned;
+            if (PlayerPrefs.GetInt("GodmodeActivated") == 1)
+            {
+                //godmodeAttiva
+                PlayerSpawned = Instantiate(playerGOD, position2, spawn2.transform.rotation);
+            }
+            else
+            {
+                //GodmodeDisattiva
+                PlayerSpawned = Instantiate(player, position2, spawn2.transform.rotation);
+            }
             hudOn = GameObject.Find("HUD").GetComponent<DetectedActDeact>();
             hudOn.thing.SetActive(true);
             //set new totalscore
@@ -45,7 +67,17 @@ public class PlayerSpawn : MonoBehaviour
         if (PlayerPrefs.GetInt("Checkpoint") == 3)
         {
             Vector3 position2 = new Vector3(spawn3.transform.position.x, spawn3.transform.position.y, spawn3.transform.position.z);
-            GameObject PlayerSpawned = Instantiate(player, position2, spawn3.transform.rotation);
+            GameObject PlayerSpawned;
+            if (PlayerPrefs.GetInt("GodmodeActivated") == 1)
+            {
+                //godmodeAttiva
+                PlayerSpawned = Instantiate(playerGOD, position2, spawn3.transform.rotation);
+            }
+            else
+            {
+                //GodmodeDisattiva
+                PlayerSpawned = Instantiate(player, position2, spawn3.transform.rotation);
+            }
             hudOn = GameObject.Find("HUD").GetComponent<DetectedActDeact>();
             hudOn.thing.SetActive(true);
             //set new totalscore
