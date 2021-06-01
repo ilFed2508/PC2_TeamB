68e889e92be9ed37d1aaa056589b2fa9bc517316
crystaldevaster <richicryst@gmail.com>
2021-06-01 09:51:28 +0200

    Script Polish and Solving Problems pt1

---
+++ ProjectTeamB_PC2/Assets/Scripts/Animation Controls/AnimationShotGun.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationShotGun : MonoBehaviour
+{
+    public Animator anim;
+
+    public string sparo;
+    public string vadoAvanti;
+    public string destra;
+    public string sinistra;
+    public string Jump;
+    private Animator Camera;
+
+    void Start()
+    {
+        Camera = GameObject.Find("CamerasLogic").GetComponent<Animator>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetMouseButton(0))
+        //{
+        //    anim.SetBool(sparo, true);
+        //}
+        //else
+        //{
+        //    anim.SetBool(sparo, false);
+        //    anim.SetBool(vadoAvanti, false);
+        //}
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool(vadoAvanti, true);
+        }
+        else
+        {
+            anim.SetBool(vadoAvanti, false);
+
+        }
+    }
+}
+
---
+++ ProjectTeamB_PC2/Assets/Scripts/Animation Controls/MinigunRotation.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigunRotation : MonoBehaviour
+{
+    /// <summary>
+    /// Turning Speed of the Weapon Barrel
+    /// </summary>
+    public float speed;
+    /// <summary>
+    /// Barrel Of the Weapon
+    /// </summary>
+    public GameObject MinigunBarrel;
+    /// <summary>
+    /// Rotation Vector to apply
+    /// </summary>
+    private Vector3 toRotateV;
+    /// <summary>
+    /// Animator Component attached to minigun
+    /// </summary>
+    private Animator MiniGunAnim;
+
+    public string sparo;
+    public string vadoAvanti;
+    public string destra;
+    public string sinistra;
+    public string Jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //minigun reference pick
+        MiniGunAnim = GetComponent<Animator>();
+        if(MiniGunAnim == null)
+        {
+            Debug.Log("Inserisci minigun Animator!");
+        }
+
+        toRotateV = new Vector3(0f,0f, MinigunBarrel.transform.localRotation.z * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MinigunBarrel.transform.Rotate(toRotateV);          
+        }
+        else
+        {
+            MiniGunAnim.SetBool("Shoot", false);
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            MiniGunAnim.SetBool(vadoAvanti, true);
+        }
+        else
+        {
+            MiniGunAnim.SetBool(vadoAvanti, false);
+
+        }
+    }
+}
