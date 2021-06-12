04c8f3f6c7f42435a7938860f5a0e6c6a971614d
Joe Abiad <Joenielscrs@gmail.com>
2021-06-12 17:06:31 +0200

    Main Menu update

---
+++ ProjectTeamB_PC2/Assets/DisplayHover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DisplayHover : MonoBehaviour
+{
+    public GameObject leaderboard, Emote;
+
+    private void Start()
+    {
+        leaderboard.SetActive(false);
+        Emote.SetActive(true);
+    }
+
+    public void OnMouseEnter()
+    {
+        leaderboard.SetActive(true);
+        Emote.SetActive(false);
+    }
+
+    public void OnMouseExit()
+    {
+        leaderboard.SetActive(false);
+        Emote.SetActive(true);
+    }
+}
+
