bc9c125bfb236c609f2bc1edd573693f17117d03
Joe Abiad <Joenielscrs@gmail.com>
2021-06-14 16:20:13 +0200

    UI Update

---
+++ ProjectTeamB_PC2/Assets/switchPowerLeadet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchPowerLeadet : MonoBehaviour
+{
+    public GameObject PowerUP, Leaderboard;
+
+    public void powerUP()
+    {
+        Leaderboard.SetActive(false);
+        PowerUP.SetActive(true);
+    }
+
+    public void leaderBoard()
+    {
+        PowerUP.SetActive(false);
+        Leaderboard.SetActive(true);
+    }
+}
