102a1ba72a90cd770f7503b31c40cfbc9358883f
LucaDL <lanzellottiluca9800@gmail.com>
2021-06-15 23:00:32 +0200

    Add Feed(Life test fade in-out)

--- ProjectTeamB_PC2/Assets/MedikitManager.cs
+++ ProjectTeamB_PC2/Assets/MedikitManager.cs
@@ -17,10 +17,19 @@ public class MedikitManager : MonoBehaviour
     [HideInInspector]
     public int NumberOfMedikitCopy;
 
+
+    [SerializeField] Image[] images = default;
+    [Min(0)]
+    [SerializeField] float waitBeforeStartFadeIn = 1;
+    [Min(0)]
+    [SerializeField] float timeToFadeIn = 1;
+    [Min(0)]
+    [SerializeField] float timeToFadeOut = 1;
+
     void Start()
     {
         MyLife = FindObjectOfType<PlayerLifeSystem>();
-        CanUseMedikit = false;
+        //CanUseMedikit = false;
         NumberOfMedikitCopy = NumberOfMedikit;
     }
 
@@ -36,6 +45,7 @@ public class MedikitManager : MonoBehaviour
             if(Input.GetMouseButtonDown(1) && NumberOfMedikit > 0)
             {
                 MyLife.PlayerCurrentHP += MedikitEffect;
+                StartCoroutine(FadeInAndOut());
                 NumberOfMedikit -= 1;
             }
         }
@@ -48,6 +58,70 @@ public class MedikitManager : MonoBehaviour
         if(!CanUseMedikit)
         {
             MedikitIcon.SetActive(false);
+        }
+    }
+
+
+
+
+
+
+
+
+    IEnumerator FadeInAndOut()
+    {
+        //start alpha to 0
+        ChangeAlpha(0);
+
+        //wait before start fade in
+        yield return new WaitForSeconds(waitBeforeStartFadeIn);
+
+        //fade in
+        float delta = 0;
+        while (delta < 1)
+        {
+            delta = Fade(0, 1, delta, timeToFadeIn);
+
+            yield return null;
+        }
+
+        //final alpha to 1
+        ChangeAlpha(1);
+
+
+        //fade out
+        delta = 0;
+        while (delta < 1)
+        {
+            delta = Fade(1, 0, delta, timeToFadeOut);
+
+            yield return null;
+        }
+
+        //final alpha to 0
+        ChangeAlpha(0);
+
+
+    }
+
+    float Fade(float from, float to, float delta, float duration)
+    {
+        //speed based to duration
+        delta += Time.deltaTime / duration;
+
+        //set alpha from to
+        float alpha = Mathf.Lerp(from, to, delta);
+        ChangeAlpha(alpha);
+
+        return delta;
+    }
+
+    void ChangeAlpha(float alpha)
+    {
+        //foreach image, change alpha
+        foreach (Image image in images)
+        {
+            image.color = new Color(image.color.r, image.color.g, image.color.b, alpha);
         }
     }
 }
