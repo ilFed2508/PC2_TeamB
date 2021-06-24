27ffdf2c7f75b2109054f19a259d383689d04736
Daniele Comotti <comotti.daniele@gmail.com>
2021-06-24 11:51:48 +0200

    SFX bonded to SFXMixer

--- ProjectTeamB_PC2/Assets/Scripts/AudioManager.cs
+++ ProjectTeamB_PC2/Assets/Scripts/AudioManager.cs
@@ -30,15 +30,15 @@ public class AudioManager : MonoBehaviour
 
     private void Awake()
     {
-        if (instance == null)
-            instance = this;
-        else
-        {
-            Destroy(gameObject);
-            return;
-        }
-            
-        DontDestroyOnLoad(gameObject);
+        //if (instance == null)
+        //    instance = this;
+        //else
+        //{
+        //    Destroy(gameObject);
+        //    return;
+        //}
+        //    
+        //DontDestroyOnLoad(gameObject);
         foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
