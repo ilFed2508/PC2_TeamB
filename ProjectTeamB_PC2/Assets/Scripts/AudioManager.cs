ec77e52cfb244326488937cd9dd365c66f519605
Daniele Comotti <comotti.daniele@gmail.com>
2021-06-24 16:53:18 +0200

    Fixed

--- ProjectTeamB_PC2/Assets/Scripts/AudioManager.cs
+++ ProjectTeamB_PC2/Assets/Scripts/AudioManager.cs
@@ -30,21 +30,22 @@ public class AudioManager : MonoBehaviour
 
     private void Awake()
     {
-        //if (instance == null)
-        //    instance = this;
-        //else
-        //{
-        //    Destroy(gameObject);
-        //    return;
-        //}
-        //    
-        //DontDestroyOnLoad(gameObject);
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+            
+        DontDestroyOnLoad(gameObject);
         foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
             s.source.volume = s.volume;
             s.source.loop = s.loop;
+            s.source.outputAudioMixerGroup = s.AudioMixer;
         }      
     }
 
