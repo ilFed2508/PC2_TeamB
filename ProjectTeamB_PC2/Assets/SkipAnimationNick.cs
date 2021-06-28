5c657e491e66a62ffe70f631369a0d8cc795c533
Joe Abiad <Joenielscrs@gmail.com>
2021-06-28 19:54:51 +0200

    New Nickname Scene

---
+++ ProjectTeamB_PC2/Assets/SkipAnimationNick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkipAnimationNick : MonoBehaviour
+{
+    public GameObject Nick, AnimationText, TextImage, FakeBackground;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Animation());
+        TextImage.SetActive(false);
+        FakeBackground.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            StopCoroutine(Animation());
+            Destroy(AnimationText);
+            FakeBackground.SetActive(false);
+            TextImage.SetActive(true);
+        }
+    }
+
+    IEnumerator Animation()
+    {
+        yield return new WaitForSeconds(4);
+        Destroy(AnimationText);
+        FakeBackground.SetActive(false);
+        TextImage.SetActive(true);
+    }
+}
