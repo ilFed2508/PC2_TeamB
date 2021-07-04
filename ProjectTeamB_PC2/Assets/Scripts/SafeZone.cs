41b141ad70709b4c759c78b85ee7668620e7c2cd
Joe Abiad <Joenielscrs@gmail.com>
2021-07-04 11:21:56 +0200

    SafeZone UI

--- ProjectTeamB_PC2/Assets/Scripts/SafeZone.cs
+++ ProjectTeamB_PC2/Assets/Scripts/SafeZone.cs
@@ -29,10 +29,11 @@ public class SafeZone : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Player") )
-        {
-            MyTime.ComboTime = 0f;
-        }
+        //if (other.CompareTag("Player"))
+        //{
+        //    MyTime.ComboTime = 0f;
+        //}
+        MyTime.ComboTime = 0f;
         if (other.CompareTag("Player") && playerController.SafeZoneReached != checkpoint)
         {
             playerController.SafeZoneReached = checkpoint;
@@ -40,7 +41,7 @@ public class SafeZone : MonoBehaviour
         if (other.CompareTag("Player") && playerController.playerLife.PlayerCurrentHP < playerController.playerLife.PlayerStartingHP)
         {
             playerController.playerLife.PlayerCurrentHP -= recoverdLife * Time.deltaTime;
-            
+            MyTime.ComboTime = 0f;
         }
     }
 
