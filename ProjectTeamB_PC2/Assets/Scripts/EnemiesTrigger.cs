17d6d54e3b687eba5945f5df9ab6dc7519e6ee92
Joe Abiad <Joenielscrs@gmail.com>
2021-07-16 14:43:33 +0200

    Polishing

--- ProjectTeamB_PC2/Assets/Scripts/EnemiesTrigger.cs
+++ ProjectTeamB_PC2/Assets/Scripts/EnemiesTrigger.cs
@@ -22,12 +22,6 @@ public class EnemiesTrigger : MonoBehaviour
         HUD = GameObject.Find("HUD");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
--- ProjectTeamB_PC2/Assets/Scripts/Scripts/EndTrigger.cs
+++ ProjectTeamB_PC2/Assets/Scripts/Scripts/EndTrigger.cs
@@ -4,18 +4,11 @@ using UnityEngine;
 
 public class EndTrigger : MonoBehaviour
 {
-    public GameObject EndMenu;
+    private GameObject EndMenu;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        EndMenu = GameObject.Find("WIN_FinalRoom");
     }
 
     private void OnTriggerStay(Collider other)
