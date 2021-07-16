109c4c4bab411cba61c9420ae99d3d744a4414e1
Daniele Comotti <comotti.daniele@gmail.com>
2021-07-16 15:34:29 +0200

    Set resolution at first start

---
+++ ProjectTeamB_PC2/Assets/__Scenes/Epilepsy Screen/FirstTimeStart.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstTimeStart : MonoBehaviour
+{
+    static int NofStarts;
+
+    private void Awake()
+    {
+        NofStarts = PlayerPrefs.GetInt("BootsUp");
+        if(NofStarts==0)
+        {
+            Screen.SetResolution(1920, 1080, Screen.fullScreen);
+        }
+        NofStarts++;
+        PlayerPrefs.SetInt("BootsUp", NofStarts);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
