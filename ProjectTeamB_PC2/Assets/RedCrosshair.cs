675ad7b49dede3b966c19f6b6959e9172b849336
LucaDL <lanzellottiluca9800@gmail.com>
2021-08-20 15:24:33 +0200

    RedCrosshairs/ARbulletsSize

---
+++ ProjectTeamB_PC2/Assets/RedCrosshair.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RedCrosshair : MonoBehaviour
+{
+    public Image crosshair;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 1f));
+        RaycastHit Hit;
+
+        if (Physics.Raycast(ray, out Hit) && Hit.collider.CompareTag("Enemy"))
+        {
+            crosshair.color = Color.red;
+        }
+        else
+        {
+            crosshair.color = Color.white;
+        }
+    }
+}
